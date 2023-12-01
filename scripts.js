7d7612137e88d11d14f21a468ce6f12e534d042f
andrewisbs <125695772+andrewisbs@users.noreply.github.com>
2023-12-01 11:06:36 -0600

    add clear button

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 const controler = document.querySelector("#controler");
 const boxes = document.querySelector("#boxes");
-const BEGINNING_BOXES = 4;
+const BEGINNING_BOXES = 15;
+const numberButton = document.querySelector("#number-button");
+const clearButton = document.querySelector("#clear-button");
+const colorButton = document.querySelector("#color-button");
+const blackButton = document.querySelector("#black-button");
     loadScreen(BEGINNING_BOXES);
 //for the initial loading
-
 
 function loadScreen(numberBoxes) {
     if (numberBoxes >30) {return;}
@@ -11,27 +14,44 @@ function loadScreen(numberBoxes) {
     for(;iteration>0; iteration--)
     {
         const box = document.createElement('div');
-        box.setAttribute("class", "box-style");
+        box.setAttribute("class", "box-style");    
+        boxes.setAttribute('draggable', 'false');
         boxes.appendChild(box);
         boxes.addEventListener("mouseover", () => {
             target = event.target
-            console.log(target);
             changeColor(target);
             addListener(boxes);
+            numberButton.addEventListener("mousedown", changeNumber);
+            clearButton.addEventListener("mousedown", clearScreen);
+            colorButton.addEventListener("mousedown", colorMode);
+            blackButton.addEventListener("mousedown", blackMode);
         });
     }
 }
-
-
-
+function changeNumber() {
+    prompt ("how many");
+}
+function clearScreen () {
+    const boxRevert = document.querySelectorAll(".box-style");
+    console.log(boxRevert);
+    for (let i=0;i<boxRevert.length; i++) {
+        boxRevert[i].style.cssText= "background-color: none";
+    }
+}
+function colorMode () {
+    //change to color mode
+}
+function blackMode () {
+    //change to black mode
+}
 function addListener (boxes) {
         boxes.addEventListener("mouseover", () => {
-        target = boxes.event.target;
-        console.log(target);
+        target = event.target;
+        event.stopImmediatePropagation();
         changeColor(target);
+
 })}
-function changeColor (boxes) {
-    boxes.style.cssText= "background-color: black";
+function changeColor (value) {
+    value.style.cssText= "background-color: black";
+    boxes.style.cssText= "background-color: none";
 }
-console.log(boxes);
-console.log(controler);
